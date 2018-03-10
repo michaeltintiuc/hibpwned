a7713c64fd6fb83b40f353dc969eccc2358e3340
Michael Tintiuc <contact@michaeltintiuc.com>
2018-03-10 05:00:05 +0200

    Refactoring. Added password pwned count

--- hibpawned.go
+++ hibpawned.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/michaeltintiuc/hibpwned/pkg/pwd"
+)
+
+var (
+	pass string
+)
+
+func init() {
+	flag.StringVar(&pass, "p", "", "Password to check")
+}
+
+func main() {
+	flag.Parse()
+	pwned, count := pwd.Check(pass)
+
+	if pwned {
+		fmt.Printf("Your password was pwned %d times\n", count)
+		os.Exit(1)
+	}
+
+	fmt.Println("You are secure, for now...")
+}
--- pkg/pwd/pwd.go
+++ pkg/pwd/pwd.go
@@ -1,72 +1,111 @@
-package main
+package pwd
 
 import (
+	"bufio"
 	"crypto/sha1"
-	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
 
 const (
-	domain = "https://api.pwnedpasswords.com/"
+	domain       = "https://api.pwnedpasswords.com/"
+	askForInput  = "Input password (hidden): "
+	emptyPassErr = "\nCan't check an empty password"
 )
 
-var (
-	pass string
-)
-
-func init() {
-	flag.StringVar(&pass, "p", "", "Password to check")
+// Pwd represents a container for the password in question
+type Pwd struct {
+	plain string
+	hash  string
+	pwned bool
+	count int
 }
 
-func main() {
-	flag.Parse()
-
-	if pass == "" {
-		fmt.Print("Input password (hidden): ")
-		p, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		pass = strings.TrimSpace(string(p))
-
-		if pass == "" {
-			fmt.Println("\nCan't check an empty password")
-			return
-		}
-
-		println()
-	}
-
-	hash := sha1.Sum([]byte(pass))
-	hashString := strings.ToUpper(fmt.Sprintf("%x", hash))
-	res, err := http.Get(domain + "range/" + hashString[:5])
-
-	if err != nil {
-		fmt.Println(err)
+// ValidatePass checks the provided password,
+// if it is an empty string - user is prompted for input.
+func (p *Pwd) ValidatePass() {
+	if p.plain != "" {
 		return
 	}
 
-	defer res.Body.Close()
-
-	data, err := ioutil.ReadAll(res.Body)
+	fmt.Print(askForInput)
+	stdin, err := terminal.ReadPassword(int(os.Stdin.Fd()))
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
 	}
 
-	if strings.Contains(string(data), hashString[5:]) {
-		fmt.Println("You've been PWNED!")
-	} else {
-		fmt.Println("You're secure, for now...")
+	p.plain = strings.TrimSpace(string(stdin))
+
+	if p.plain == "" {
+		fmt.Fprintln(os.Stderr, emptyPassErr)
+		os.Exit(2)
 	}
+
+	fmt.Println()
+}
+
+// Hash creates a SHA-1 hash of the password
+func (p *Pwd) Hash() {
+	hash := sha1.Sum([]byte(p.plain))
+	p.hash = strings.ToUpper(fmt.Sprintf("%x", hash))
+}
+
+// Search if first 5 characters of the SHA-1 hash
+// are found in the list of comporomised passwords
+func (p *Pwd) Search() {
+	pwned := FetchPwned(p.hash[:5])
+	scanner := bufio.NewScanner(pwned)
+	hashPart := p.hash[5:]
+	defer pwned.Close()
+
+	for scanner.Scan() {
+		if row := scanner.Text(); strings.Contains(row, hashPart) {
+			p.pwned = true
+
+			count, err := strconv.ParseFloat(strings.Split(row, ":")[1], 10)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				defer func() { pwned.Close(); os.Exit(2) }()
+			}
+
+			p.count = int(count)
+			return
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+// Check if given password was compromised or not.
+func Check(pass string) (bool, int) {
+	p := Pwd{pass, "", false, 0}
+	p.ValidatePass()
+	p.Hash()
+	p.Search()
+
+	return p.pwned, p.count
+}
+
+// FetchPwned sends a request to the HIBPwned API
+// and returns list of of compromised passwords
+func FetchPwned(hash string) io.ReadCloser {
+	res, err := http.Get(domain + "range/" + hash)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+
+	return res.Body
 }
